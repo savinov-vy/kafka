ccd793b786451a5bc0d1b37d0bd35615434f5c67
Savinov <savinov_vu@mail.ru>
2024-06-05 14:47:32 +0800

    init commit

---
+++ middleware/src/main/java/ru/savinov/middleware/MiddlewareApplication.java
@@ -0,0 +1,13 @@
+package ru.savinov.middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddlewareApplication.class, args);
+	}
+
+}
