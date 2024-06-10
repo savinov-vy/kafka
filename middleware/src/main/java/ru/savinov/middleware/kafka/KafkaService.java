7c368381ac6c94f9101e5e48e1b98ca9535f8984
Savinov <savinov_vu@mail.ru>
2024-06-10 17:46:55 +0800

    connect middleware to database

--- middleware/src/main/java/ru/savinov/middleware/kafka/KafkaService.java
+++ middleware/src/main/java/ru/savinov/middleware/kafka/KafkaService.java
@@ -2,6 +2,9 @@ package ru.savinov.middleware.kafka;
 
 import lombok.AllArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,8 +12,11 @@ import org.springframework.stereotype.Service;
 public class KafkaService {
 
     @KafkaListener(topics = "basket", groupId = "group_id")
-    public void consume(String message) {
+    public void consume(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
+                        @Header(KafkaHeaders.OFFSET) long offset) {
         System.out.println(message);
+        System.out.println("Partition: " + partition);
+        System.out.println("Offset: " + offset);
     }
 
 }
